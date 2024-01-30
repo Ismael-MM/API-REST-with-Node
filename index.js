41f54e4ae9dd03123eaf8b361afc72e985572d27
ismael <ismontelongo@outlook.com>
2024-01-30 17:52:45 +0000

    esquema mongoose y morgan

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
+import mongoose from 'mongoose';
 const express = require('express')
+const morgan = require('morgan')
+const { Schema } = mongoose;
+
+
 const app = express()
 const port = 3000
 
+const account = new Schema({
+    name: String, // String is shorthand for {type: String}
+    balance: String,
+  });
+
+
+app.use(morgan('combined'))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/accounts', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/accounts', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.put('/accounts/:id', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.delete('/accounts/:id', (req, res) => {
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
